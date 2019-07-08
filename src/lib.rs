c6b41cbe27fb56a1cbbf5513167ba44a7cfbe64e
mycognosist <gnomad@cryptolab.net>
2019-07-08 11:06:27 +0100

    Separate buttons client into module

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ extern crate crossbeam_channel;
 extern crate jsonrpc_client_core;
 extern crate ws;
 
+mod buttons;
 mod error;
 mod oled;
 
@@ -20,15 +21,12 @@ use std::{env, process, thread};
 use crossbeam_channel::unbounded;
 use crossbeam_channel::*;
 
-use jsonrpc_http_server::jsonrpc_core::*;
 #[allow(unused_imports)]
 use jsonrpc_test as test;
 
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use ws::connect;
 
-use ws::{connect, CloseCode, Error, Handler, Handshake, Message, Sender};
-
+use crate::buttons::*;
 use crate::oled::*;
 
 /// Initializes the state machine, listens for button events and drives
@@ -101,18 +99,6 @@ pub fn run() -> std::result::Result<(), Box<dyn std::error::Error>> {
     connect(ws_server, |out| Client { out, s: s2 })?;
 
     Ok(())
-}
-
-#[derive(Debug, Serialize)]
-pub struct SubscribeMsg {
-    pub id: u8,
-    pub jsonrpc: String,
-    pub method: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Press {
-    pub button_code: u8,
 }
 
 #[derive(Debug, PartialEq)]
@@ -223,77 +209,6 @@ impl State {
                 error!("State machine failed during run method.");
             }
         }
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct ButtonMsg {
-    jsonrpc: String,
-    method: String,
-    params: Vec<u8>,
-}
-
-// websocket client
-#[derive(Debug)]
-pub struct Client<'a> {
-    out: Sender,
-    s: &'a crossbeam_channel::Sender<u8>,
-}
-
-impl<'a> Handler for Client<'a> {
-    /// Sends request to `peach_buttons` to subscribe to emitted events.
-    fn on_open(&mut self, _: Handshake) -> ws::Result<()> {
-        info!("Subscribing to peach_buttons microservice over ws.");
-        let subscribe = json!({
-            "id":1,
-            "jsonrpc":"2.0",
-            "method":"subscribe_buttons"
-        });
-        let data = subscribe.to_string();
-        self.out.send(data)
-    }
-
-    /// Displays JSON-RPC request from `peach_buttons`.
-    fn on_message(&mut self, msg: Message) -> ws::Result<()> {
-        info!("Received ws message from peach_buttons.");
-        // button_code must be extracted from the request and passed to
-        // state_changer
-        let m: String = msg.into_text()?;
-        // distinguish button_press events from other received jsonrpc requests
-        if m.contains(r"params") {
-            // serialize msg string into a struct
-            let bm: ButtonMsg = serde_json::from_str(&m).unwrap_or_else(|err| {
-                error!("Problem serializing button_code msg: {}", err);
-                process::exit(1);
-            });
-            debug!("Sending button code to state_changer.");
-            // send the button_code parameter to state_changer
-            self.s.send(bm.params[0]).unwrap_or_else(|err| {
-                error!("Problem sending button_code over channel: {}", err);
-                process::exit(1);
-            });
-        }
-        Ok(())
-    }
-
-    /// Handles disconnection from websocket and displays debug data.
-    fn on_close(&mut self, code: CloseCode, reason: &str) {
-        match code {
-            CloseCode::Normal => {
-                info!("The client is done with the connection.");
-            }
-            CloseCode::Away => {
-                info!("The client is leaving the site.");
-            }
-            CloseCode::Abnormal => {
-                warn!("Closing handshake failed! Unable to obtain closing status from client.");
-            }
-            _ => error!("The client encountered an error: {}", reason),
-        }
-    }
-
-    fn on_error(&mut self, err: Error) {
-        error!("The server encountered an error: {:?}", err);
     }
 }
 
